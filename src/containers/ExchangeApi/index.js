f7f409ef6c52e367f4d786218557ead1f31c3eb1
Jackie Tu <jackie.tu@btcc.com>
2017-12-27 17:53:20 +0800

    nothing to do

--- src/containers/ExchangeApi/index.js
+++ src/containers/ExchangeApi/index.js
@@ -1,5 +1,38 @@
 import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
+import { Table, Select } from 'antd';
+import setting from '../../../setting.json';
+import { exchange } from '../../mock/Exchange';
+
+const { Option } = Select;
+
+const columns = [{
+  title: 'Market',
+  dataIndex: 'market',
+  key: 'market',
+}, {
+  title: 'Currency',
+  dataIndex: 'id',
+  key: 'currency',
+}, {
+  title: 'Price',
+  dataIndex: 'last',
+  key: 'price',
+}];
+
+const childColumns = [{
+  title: 'Market',
+  dataIndex: 'market',
+  key: 'market',
+}, {
+  title: 'Price',
+  dataIndex: 'last',
+  key: 'price',
+}, {
+  title: 'Change',
+  dataIndex: 'change',
+  key: 'change',
+}];
 
 @connect(
   null,
@@ -9,13 +42,65 @@ import { connect } from 'react-redux';
 )
 
 export default class ExchangeApi extends PureComponent {
-  componentWillMount() {
-    // const { _connectApi } = this.props;
-    // _connectApi();
+  state = {
+    exchangeData: setting.defaultExchange,
+  }
+  _expandedRowRender = (record) => {
+    const newExchange = Object.assign({}, exchange, {
+      [this.state.exchangeData]: [],
+    });
+    const n = [];
+    for (let i = 0, len = Object.values(newExchange).length; i < len; i += 1) {
+      Object.values(newExchange)[i].filter((val) => {
+        const m = val;
+        if (m.id === record.id) {
+          m.key = i;
+          m.market = Object.keys(newExchange)[i];
+          m.change = Number(record.last) - Number(val.last);
+          return n.push(val);
+        }
+        return false;
+      });
+    }
+    const data = n;
+    return (
+      <Table
+        columns={childColumns}
+        dataSource={data}
+        pagination={false}
+      />
+    );
+  }
+  _selectHandleChange = v => this.setState({ exchangeData: v });
+
+  _exchangeData = (exchangeData) => {
+    const data = [];
+    for (let i = 0, len = exchange[exchangeData].length; i < len; i += 1) {
+      exchange[exchangeData][i].key = i;
+      exchange[exchangeData][i].market = exchangeData;
+      data.push(exchange[exchangeData][i]);
+    }
+    return data;
   }
   render() {
+    const { exchangeData } = this.state;
     return (
-      <div>Table</div>
+      <div>
+        Table
+        <br />
+        <Select
+          defaultValue={exchangeData}
+          style={{ width: 120 }}
+          onChange={this._selectHandleChange}
+        >
+          {setting.exchange.map(v => <Option key={v} value={v}>{v}</Option>)}
+        </Select>
+        <Table
+          columns={columns}
+          dataSource={this._exchangeData(exchangeData)}
+          expandedRowRender={this._expandedRowRender}
+        />
+      </div>
     );
   }
 }
---
+++ src/mock/Exchange.js
@@ -0,0 +1,1195 @@
+export const exchange = {
+  bitfinex: [{
+    id: 'BTC_BCN',
+    last: '0.00000042',
+  },
+  {
+    id: 'BTC_BELA',
+    last: '0.00001789',
+  },
+  {
+    id: 'BTC_BLK',
+    last: '0.00003768',
+  },
+  {
+    id: 'BTC_BTCD',
+    last: '0.01740480',
+  },
+  {
+    id: 'BTC_BTM',
+    last: '0.00014647',
+  },
+  {
+    id: 'BTC_BTS',
+    last: '0.00003381',
+  },
+  {
+    id: 'BTC_BURST',
+    last: '0.00000479',
+  },
+  {
+    id: 'BTC_CLAM',
+    last: '0.00061501',
+  },
+  {
+    id: 'BTC_DASH',
+    last: '0.07369814',
+  },
+  {
+    id: 'BTC_DGB',
+    last: '0.00000384',
+  },
+  {
+    id: 'BTC_DOGE',
+    last: '0.00000057',
+  },
+  {
+    id: 'BTC_EMC2',
+    last: '0.00005378',
+  },
+  {
+    id: 'BTC_FLDC',
+    last: '0.00000339',
+  },
+  {
+    id: 'BTC_FLO',
+    last: '0.00001263',
+  },
+  {
+    id: 'BTC_GAME',
+    last: '0.00025000',
+  },
+  {
+    id: 'BTC_GRC',
+    last: '0.00000634',
+  },
+  {
+    id: 'BTC_HUC',
+    last: '0.00001902',
+  },
+  {
+    id: 'BTC_LTC',
+    last: '0.01710974',
+  },
+  {
+    id: 'BTC_MAID',
+    last: '0.00005190',
+  },
+  {
+    id: 'BTC_OMNI',
+    last: '0.00465156',
+  },
+  {
+    id: 'BTC_NAV',
+    last: '0.00019350',
+  },
+  {
+    id: 'BTC_NEOS',
+    last: '0.00051735',
+  },
+  {
+    id: 'BTC_NMC',
+    last: '0.00025488',
+  },
+  {
+    id: 'BTC_NXT',
+    last: '0.00008142',
+  },
+  {
+    id: 'BTC_PINK',
+    last: '0.00000320',
+  },
+  {
+    id: 'BTC_POT',
+    last: '0.00002078',
+  },
+  {
+    id: 'BTC_PPC',
+    last: '0.00030600',
+  },
+  {
+    id: 'BTC_RIC',
+    last: '0.00001470',
+  },
+  {
+    id: 'BTC_STR',
+    last: '0.00001361',
+  },
+  {
+    id: 'BTC_SYS',
+    last: '0.00005245',
+  },
+  {
+    id: 'BTC_VIA',
+    last: '0.00026839',
+  },
+  {
+    id: 'BTC_XVC',
+    last: '0.00005514',
+  },
+  {
+    id: 'BTC_VRC',
+    last: '0.00006001',
+  },
+  {
+    id: 'BTC_VTC',
+    last: '0.00044021',
+  },
+  {
+    id: 'BTC_XBC',
+    last: '0.00720000',
+  },
+  {
+    id: 'BTC_XCP',
+    last: '0.00196983',
+  },
+  {
+    id: 'BTC_XEM',
+    last: '0.00005882',
+  },
+  {
+    id: 'BTC_XMR',
+    last: '0.02419191',
+  },
+  {
+    id: 'BTC_XPM',
+    last: '0.00005146',
+  },
+  {
+    id: 'BTC_XRP',
+    last: '0.00006770',
+  },
+  {
+    id: 'USDT_BTC',
+    last: '16489.99999999',
+  },
+  {
+    id: 'USDT_DASH',
+    last: '1207.65207592',
+  },
+  {
+    id: 'USDT_LTC',
+    last: '281.50000001',
+  },
+  {
+    id: 'USDT_NXT',
+    last: '1.34399992',
+  },
+  {
+    id: 'USDT_STR',
+    last: '0.22450000',
+  },
+  {
+    id: 'USDT_XMR',
+    last: '399.54547097',
+  },
+  {
+    id: 'USDT_XRP',
+    last: '1.11314993',
+  },
+  {
+    id: 'XMR_BCN',
+    last: '0.00001738',
+  },
+  {
+    id: 'XMR_BLK',
+    last: '0.00157511',
+  },
+  {
+    id: 'XMR_BTCD',
+    last: '0.72520011',
+  },
+  {
+    id: 'XMR_DASH',
+    last: '3.04654789',
+  },
+  {
+    id: 'XMR_LTC',
+    last: '0.70673992',
+  },
+  {
+    id: 'XMR_MAID',
+    last: '0.00214961',
+  },
+  {
+    id: 'XMR_NXT',
+    last: '0.00342260',
+  },
+  {
+    id: 'BTC_ETH',
+    last: '0.04634003',
+  },
+  {
+    id: 'USDT_ETH',
+    last: '764.00000000',
+  },
+  {
+    id: 'BTC_SC',
+    last: '0.00000167',
+  },
+  {
+    id: 'BTC_BCY',
+    last: '0.00004879',
+  },
+  {
+    id: 'BTC_EXP',
+    last: '0.00042841',
+  },
+  {
+    id: 'BTC_FCT',
+    last: '0.00269374',
+  },
+  {
+    id: 'BTC_RADS',
+    last: '0.00075401',
+  },
+  {
+    id: 'BTC_AMP',
+    last: '0.00003263',
+  },
+  {
+    id: 'BTC_DCR',
+    last: '0.00572354',
+  },
+  {
+    id: 'BTC_LSK',
+    last: '0.00146032',
+  },
+  {
+    id: 'ETH_LSK',
+    last: '0.03114646',
+  },
+  {
+    id: 'BTC_LBC',
+    last: '0.00004101',
+  },
+  {
+    id: 'BTC_STEEM',
+    last: '0.00019669',
+  },
+  {
+    id: 'ETH_STEEM',
+    last: '0.00433750',
+  },
+  {
+    id: 'BTC_SBD',
+    last: '0.00054090',
+  },
+  {
+    id: 'BTC_ETC',
+    last: '0.00183707',
+  },
+  {
+    id: 'ETH_ETC',
+    last: '0.03995154',
+  },
+  {
+    id: 'USDT_ETC',
+    last: '30.30000001',
+  },
+  {
+    id: 'BTC_REP',
+    last: '0.00505246',
+  },
+  {
+    id: 'USDT_REP',
+    last: '83.84799996',
+  },
+  {
+    id: 'ETH_REP',
+    last: '0.10836978',
+  },
+  {
+    id: 'BTC_ARDR',
+    last: '0.00009404',
+  },
+  {
+    id: 'BTC_ZEC',
+    last: '0.03379000',
+  },
+  {
+    id: 'ETH_ZEC',
+    last: '0.72426740',
+  },
+  {
+    id: 'USDT_ZEC',
+    last: '560.00000000',
+  },
+  {
+    id: 'XMR_ZEC',
+    last: '1.43957072',
+  },
+  {
+    id: 'BTC_STRAT',
+    last: '0.00084955',
+  },
+  {
+    id: 'BTC_NXC',
+    last: '0.00002496',
+  },
+  {
+    id: 'BTC_PASC',
+    last: '0.00014400',
+  },
+  {
+    id: 'BTC_GNT',
+    last: '0.00004680',
+  },
+  {
+    id: 'ETH_GNT',
+    last: '0.00100994',
+  },
+  {
+    id: 'BTC_GNO',
+    last: '0.01249301',
+  },
+  {
+    id: 'ETH_GNO',
+    last: '0.26658000',
+  },
+  {
+    id: 'BTC_BCH',
+    last: '0.17402102',
+  },
+  {
+    id: 'ETH_BCH',
+    last: '3.79999850',
+  },
+  {
+    id: 'USDT_BCH',
+    last: '2874.99999999',
+  },
+  {
+    id: 'BTC_ZRX',
+    last: '0.00003886',
+  },
+  {
+    id: 'ETH_ZRX',
+    last: '0.00083404',
+  },
+  {
+    id: 'BTC_CVC',
+    last: '0.00005603',
+  },
+  {
+    id: 'ETH_CVC',
+    last: '0.00119149',
+  },
+  {
+    id: 'BTC_OMG',
+    last: '0.00094864',
+  },
+  {
+    id: 'ETH_OMG',
+    last: '0.02038631',
+  },
+  {
+    id: 'BTC_GAS',
+    last: '0.00190014',
+  },
+  {
+    id: 'ETH_GAS',
+    last: '0.04102315',
+  },
+  {
+    id: 'BTC_STORJ',
+    last: '0.00008773',
+  },
+  ],
+  'gate.io': [{
+    id: 'BTC_BCN',
+    last: '0.00000042',
+  },
+  {
+    id: 'BTC_BELA',
+    last: '0.00001789',
+  },
+  {
+    id: 'BTC_BLK',
+    last: '0.00003768',
+  },
+  {
+    id: 'BTC_BTCD',
+    last: '0.01740480',
+  },
+  {
+    id: 'BTC_BTM',
+    last: '0.00014647',
+  },
+  {
+    id: 'BTC_BTS',
+    last: '0.00003381',
+  },
+  {
+    id: 'BTC_BURST',
+    last: '0.00000479',
+  },
+  {
+    id: 'BTC_CLAM',
+    last: '0.00061501',
+  },
+  {
+    id: 'BTC_DASH',
+    last: '0.07369814',
+  },
+  {
+    id: 'BTC_DGB',
+    last: '0.00000384',
+  },
+  {
+    id: 'BTC_DOGE',
+    last: '0.00000057',
+  },
+  {
+    id: 'BTC_EMC2',
+    last: '0.00005378',
+  },
+  {
+    id: 'BTC_FLDC',
+    last: '0.00000339',
+  },
+  {
+    id: 'BTC_FLO',
+    last: '0.00001263',
+  },
+  {
+    id: 'BTC_GAME',
+    last: '0.00025000',
+  },
+  {
+    id: 'BTC_GRC',
+    last: '0.00000634',
+  },
+  {
+    id: 'BTC_HUC',
+    last: '0.00001902',
+  },
+  {
+    id: 'BTC_LTC',
+    last: '0.01710974',
+  },
+  {
+    id: 'BTC_MAID',
+    last: '0.00005190',
+  },
+  {
+    id: 'BTC_OMNI',
+    last: '0.00465156',
+  },
+  {
+    id: 'BTC_NAV',
+    last: '0.00019350',
+  },
+  {
+    id: 'BTC_NEOS',
+    last: '0.00051735',
+  },
+  {
+    id: 'BTC_NMC',
+    last: '0.00025488',
+  },
+  {
+    id: 'BTC_NXT',
+    last: '0.00008142',
+  },
+  {
+    id: 'BTC_PINK',
+    last: '0.00000320',
+  },
+  {
+    id: 'BTC_POT',
+    last: '0.00002078',
+  },
+  {
+    id: 'BTC_PPC',
+    last: '0.00030600',
+  },
+  {
+    id: 'BTC_RIC',
+    last: '0.00001470',
+  },
+  {
+    id: 'BTC_STR',
+    last: '0.00001361',
+  },
+  {
+    id: 'BTC_SYS',
+    last: '0.00005245',
+  },
+  {
+    id: 'BTC_VIA',
+    last: '0.00026839',
+  },
+  {
+    id: 'BTC_XVC',
+    last: '0.00005514',
+  },
+  {
+    id: 'BTC_VRC',
+    last: '0.00006001',
+  },
+  {
+    id: 'BTC_VTC',
+    last: '0.00044021',
+  },
+  {
+    id: 'BTC_XBC',
+    last: '0.00720000',
+  },
+  {
+    id: 'BTC_XCP',
+    last: '0.00196983',
+  },
+  {
+    id: 'BTC_XEM',
+    last: '0.00005882',
+  },
+  {
+    id: 'BTC_XMR',
+    last: '0.02419191',
+  },
+  {
+    id: 'BTC_XPM',
+    last: '0.00005146',
+  },
+  {
+    id: 'BTC_XRP',
+    last: '0.00006770',
+  },
+  {
+    id: 'USDT_BTC',
+    last: '16489.99999999',
+  },
+  {
+    id: 'USDT_DASH',
+    last: '1207.65207592',
+  },
+  {
+    id: 'USDT_LTC',
+    last: '281.50000001',
+  },
+  {
+    id: 'USDT_NXT',
+    last: '1.34399992',
+  },
+  {
+    id: 'USDT_STR',
+    last: '0.22450000',
+  },
+  {
+    id: 'USDT_XMR',
+    last: '399.54547097',
+  },
+  {
+    id: 'USDT_XRP',
+    last: '1.11314993',
+  },
+  {
+    id: 'XMR_BCN',
+    last: '0.00001738',
+  },
+  {
+    id: 'XMR_BLK',
+    last: '0.00157511',
+  },
+  {
+    id: 'XMR_BTCD',
+    last: '0.72520011',
+  },
+  {
+    id: 'XMR_DASH',
+    last: '3.04654789',
+  },
+  {
+    id: 'XMR_LTC',
+    last: '0.70673992',
+  },
+  {
+    id: 'XMR_MAID',
+    last: '0.00214961',
+  },
+  {
+    id: 'XMR_NXT',
+    last: '0.00342260',
+  },
+  {
+    id: 'BTC_ETH',
+    last: '0.04634003',
+  },
+  {
+    id: 'USDT_ETH',
+    last: '764.00000000',
+  },
+  {
+    id: 'BTC_SC',
+    last: '0.00000167',
+  },
+  {
+    id: 'BTC_BCY',
+    last: '0.00004879',
+  },
+  {
+    id: 'BTC_EXP',
+    last: '0.00042841',
+  },
+  {
+    id: 'BTC_FCT',
+    last: '0.00269374',
+  },
+  {
+    id: 'BTC_RADS',
+    last: '0.00075401',
+  },
+  {
+    id: 'BTC_AMP',
+    last: '0.00003263',
+  },
+  {
+    id: 'BTC_DCR',
+    last: '0.00572354',
+  },
+  {
+    id: 'BTC_LSK',
+    last: '0.00146032',
+  },
+  {
+    id: 'ETH_LSK',
+    last: '0.03114646',
+  },
+  {
+    id: 'BTC_LBC',
+    last: '0.00004101',
+  },
+  {
+    id: 'BTC_STEEM',
+    last: '0.00019669',
+  },
+  {
+    id: 'ETH_STEEM',
+    last: '0.00433750',
+  },
+  {
+    id: 'BTC_SBD',
+    last: '0.00054090',
+  },
+  {
+    id: 'BTC_ETC',
+    last: '0.00183707',
+  },
+  {
+    id: 'ETH_ETC',
+    last: '0.03995154',
+  },
+  {
+    id: 'USDT_ETC',
+    last: '30.30000001',
+  },
+  {
+    id: 'BTC_REP',
+    last: '0.00505246',
+  },
+  {
+    id: 'USDT_REP',
+    last: '83.84799996',
+  },
+  {
+    id: 'ETH_REP',
+    last: '0.10836978',
+  },
+  {
+    id: 'BTC_ARDR',
+    last: '0.00009404',
+  },
+  {
+    id: 'BTC_ZEC',
+    last: '0.03379000',
+  },
+  {
+    id: 'ETH_ZEC',
+    last: '0.72426740',
+  },
+  {
+    id: 'USDT_ZEC',
+    last: '560.00000000',
+  },
+  {
+    id: 'XMR_ZEC',
+    last: '1.43957072',
+  },
+  {
+    id: 'BTC_STRAT',
+    last: '0.00084955',
+  },
+  {
+    id: 'BTC_NXC',
+    last: '0.00002496',
+  },
+  {
+    id: 'BTC_PASC',
+    last: '0.00014400',
+  },
+  {
+    id: 'BTC_GNT',
+    last: '0.00004680',
+  },
+  {
+    id: 'ETH_GNT',
+    last: '0.00100994',
+  },
+  {
+    id: 'BTC_GNO',
+    last: '0.01249301',
+  },
+  {
+    id: 'ETH_GNO',
+    last: '0.26658000',
+  },
+  {
+    id: 'BTC_BCH',
+    last: '0.17402102',
+  },
+  {
+    id: 'ETH_BCH',
+    last: '3.79999850',
+  },
+  {
+    id: 'USDT_BCH',
+    last: '2874.99999999',
+  },
+  {
+    id: 'BTC_ZRX',
+    last: '0.00003886',
+  },
+  {
+    id: 'ETH_ZRX',
+    last: '0.00083404',
+  },
+  {
+    id: 'BTC_CVC',
+    last: '0.00005603',
+  },
+  {
+    id: 'ETH_CVC',
+    last: '0.00119149',
+  },
+  {
+    id: 'BTC_OMG',
+    last: '0.00094864',
+  },
+  {
+    id: 'ETH_OMG',
+    last: '0.02038631',
+  },
+  {
+    id: 'BTC_GAS',
+    last: '0.00190014',
+  },
+  {
+    id: 'ETH_GAS',
+    last: '0.04102315',
+  },
+  {
+    id: 'BTC_STORJ',
+    last: '0.00008773',
+  },
+  ],
+  poloniex: [{
+    id: 'BTC_BCN',
+    last: '0.00000042',
+  },
+  {
+    id: 'BTC_BELA',
+    last: '0.00001789',
+  },
+  {
+    id: 'BTC_BLK',
+    last: '0.00003768',
+  },
+  {
+    id: 'BTC_BTCD',
+    last: '0.01740480',
+  },
+  {
+    id: 'BTC_BTM',
+    last: '0.00014647',
+  },
+  {
+    id: 'BTC_BTS',
+    last: '0.00003381',
+  },
+  {
+    id: 'BTC_BURST',
+    last: '0.00000479',
+  },
+  {
+    id: 'BTC_CLAM',
+    last: '0.00061501',
+  },
+  {
+    id: 'BTC_DASH',
+    last: '0.07369814',
+  },
+  {
+    id: 'BTC_DGB',
+    last: '0.00000384',
+  },
+  {
+    id: 'BTC_DOGE',
+    last: '0.00000057',
+  },
+  {
+    id: 'BTC_EMC2',
+    last: '0.00005378',
+  },
+  {
+    id: 'BTC_FLDC',
+    last: '0.00000339',
+  },
+  {
+    id: 'BTC_FLO',
+    last: '0.00001263',
+  },
+  {
+    id: 'BTC_GAME',
+    last: '0.00025000',
+  },
+  {
+    id: 'BTC_GRC',
+    last: '0.00000634',
+  },
+  {
+    id: 'BTC_HUC',
+    last: '0.00001902',
+  },
+  {
+    id: 'BTC_LTC',
+    last: '0.01710974',
+  },
+  {
+    id: 'BTC_MAID',
+    last: '0.00005190',
+  },
+  {
+    id: 'BTC_OMNI',
+    last: '0.00465156',
+  },
+  {
+    id: 'BTC_NAV',
+    last: '0.00019350',
+  },
+  {
+    id: 'BTC_NEOS',
+    last: '0.00051735',
+  },
+  {
+    id: 'BTC_NMC',
+    last: '0.00025488',
+  },
+  {
+    id: 'BTC_NXT',
+    last: '0.00008142',
+  },
+  {
+    id: 'BTC_PINK',
+    last: '0.00000320',
+  },
+  {
+    id: 'BTC_POT',
+    last: '0.00002078',
+  },
+  {
+    id: 'BTC_PPC',
+    last: '0.00030600',
+  },
+  {
+    id: 'BTC_RIC',
+    last: '0.00001470',
+  },
+  {
+    id: 'BTC_STR',
+    last: '0.00001361',
+  },
+  {
+    id: 'BTC_SYS',
+    last: '0.00005245',
+  },
+  {
+    id: 'BTC_VIA',
+    last: '0.00026839',
+  },
+  {
+    id: 'BTC_XVC',
+    last: '0.00005514',
+  },
+  {
+    id: 'BTC_VRC',
+    last: '0.00006001',
+  },
+  {
+    id: 'BTC_VTC',
+    last: '0.00044021',
+  },
+  {
+    id: 'BTC_XBC',
+    last: '0.00720000',
+  },
+  {
+    id: 'BTC_XCP',
+    last: '0.00196983',
+  },
+  {
+    id: 'BTC_XEM',
+    last: '0.00005882',
+  },
+  {
+    id: 'BTC_XMR',
+    last: '0.02419191',
+  },
+  {
+    id: 'BTC_XPM',
+    last: '0.00005146',
+  },
+  {
+    id: 'BTC_XRP',
+    last: '0.00006770',
+  },
+  {
+    id: 'USDT_BTC',
+    last: '16489.99999999',
+  },
+  {
+    id: 'USDT_DASH',
+    last: '1207.65207592',
+  },
+  {
+    id: 'USDT_LTC',
+    last: '281.50000001',
+  },
+  {
+    id: 'USDT_NXT',
+    last: '1.34399992',
+  },
+  {
+    id: 'USDT_STR',
+    last: '0.22450000',
+  },
+  {
+    id: 'USDT_XMR',
+    last: '399.54547097',
+  },
+  {
+    id: 'USDT_XRP',
+    last: '1.11314993',
+  },
+  {
+    id: 'XMR_BCN',
+    last: '0.00001738',
+  },
+  {
+    id: 'XMR_BLK',
+    last: '0.00157511',
+  },
+  {
+    id: 'XMR_BTCD',
+    last: '0.72520011',
+  },
+  {
+    id: 'XMR_DASH',
+    last: '3.04654789',
+  },
+  {
+    id: 'XMR_LTC',
+    last: '0.70673992',
+  },
+  {
+    id: 'XMR_MAID',
+    last: '0.00214961',
+  },
+  {
+    id: 'XMR_NXT',
+    last: '0.00342260',
+  },
+  {
+    id: 'BTC_ETH',
+    last: '0.04634003',
+  },
+  {
+    id: 'USDT_ETH',
+    last: '764.00000000',
+  },
+  {
+    id: 'BTC_SC',
+    last: '0.00000167',
+  },
+  {
+    id: 'BTC_BCY',
+    last: '0.00004879',
+  },
+  {
+    id: 'BTC_EXP',
+    last: '0.00042841',
+  },
+  {
+    id: 'BTC_FCT',
+    last: '0.00269374',
+  },
+  {
+    id: 'BTC_RADS',
+    last: '0.00075401',
+  },
+  {
+    id: 'BTC_AMP',
+    last: '0.00003263',
+  },
+  {
+    id: 'BTC_DCR',
+    last: '0.00572354',
+  },
+  {
+    id: 'BTC_LSK',
+    last: '0.00146032',
+  },
+  {
+    id: 'ETH_LSK',
+    last: '0.03114646',
+  },
+  {
+    id: 'BTC_LBC',
+    last: '0.00004101',
+  },
+  {
+    id: 'BTC_STEEM',
+    last: '0.00019669',
+  },
+  {
+    id: 'ETH_STEEM',
+    last: '0.00433750',
+  },
+  {
+    id: 'BTC_SBD',
+    last: '0.00054090',
+  },
+  {
+    id: 'BTC_ETC',
+    last: '0.00183707',
+  },
+  {
+    id: 'ETH_ETC',
+    last: '0.03995154',
+  },
+  {
+    id: 'USDT_ETC',
+    last: '30.30000001',
+  },
+  {
+    id: 'BTC_REP',
+    last: '0.00505246',
+  },
+  {
+    id: 'USDT_REP',
+    last: '83.84799996',
+  },
+  {
+    id: 'ETH_REP',
+    last: '0.10836978',
+  },
+  {
+    id: 'BTC_ARDR',
+    last: '0.00009404',
+  },
+  {
+    id: 'BTC_ZEC',
+    last: '0.03379000',
+  },
+  {
+    id: 'ETH_ZEC',
+    last: '0.72426740',
+  },
+  {
+    id: 'USDT_ZEC',
+    last: '560.00000000',
+  },
+  {
+    id: 'XMR_ZEC',
+    last: '1.43957072',
+  },
+  {
+    id: 'BTC_STRAT',
+    last: '0.00084955',
+  },
+  {
+    id: 'BTC_NXC',
+    last: '0.00002496',
+  },
+  {
+    id: 'BTC_PASC',
+    last: '0.00014400',
+  },
+  {
+    id: 'BTC_GNT',
+    last: '0.00004680',
+  },
+  {
+    id: 'ETH_GNT',
+    last: '0.00100994',
+  },
+  {
+    id: 'BTC_GNO',
+    last: '0.01249301',
+  },
+  {
+    id: 'ETH_GNO',
+    last: '0.26658000',
+  },
+  {
+    id: 'BTC_BCH',
+    last: '0.17402102',
+  },
+  {
+    id: 'ETH_BCH',
+    last: '3.79999850',
+  },
+  {
+    id: 'USDT_BCH',
+    last: '2874.99999999',
+  },
+  {
+    id: 'BTC_ZRX',
+    last: '0.00003886',
+  },
+  {
+    id: 'ETH_ZRX',
+    last: '0.00083404',
+  },
+  {
+    id: 'BTC_CVC',
+    last: '0.00005603',
+  },
+  {
+    id: 'ETH_CVC',
+    last: '0.00119149',
+  },
+  {
+    id: 'BTC_OMG',
+    last: '0.00094864',
+  },
+  {
+    id: 'ETH_OMG',
+    last: '0.02038631',
+  },
+  {
+    id: 'BTC_GAS',
+    last: '0.00190014',
+  },
+  {
+    id: 'ETH_GAS',
+    last: '0.04102315',
+  },
+  {
+    id: 'BTC_STORJ',
+    last: '0.00008773',
+  },
+  ],
+};
+
+export default exchange;
