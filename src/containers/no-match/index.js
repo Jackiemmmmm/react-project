3d41e83719b201e187f8e6a302fdc373ad28c296
Jackiemmmmm <282301059@qq.com>
2018-07-03 16:14:25 +0800

    feat: add new 404 page for pure css

--- src/containers/no-match/index.js
+++ src/containers/no-match/index.js
@@ -1,24 +1,57 @@
 import React, { PureComponent } from 'react';
+import styles from './styles.css';
 
 class NoMatch extends PureComponent {
-  state = {
-    time: 3,
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: 5,
+    };
+    this.timeout = null;
+    this.renderTimeout = null;
   }
 
   componentWillMount() {
     const { history } = this.props;
-    setTimeout(() => {
-      history.goBack();
-    }, 3000);
+    for (let i = 1; i < 6; i += 1) {
+      this.renderTimeout = setTimeout(() => {
+        const { time } = this.state;
+        this.setState({ time: time - 1 });
+      }, i * 1000);
+    }
+    this.timeout = setTimeout(() => {
+      history.replace('/');
+    }, 6000);
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.timeout);
+    clearTimeout(this.renderTimeout);
   }
 
   render() {
     const { time } = this.state;
     return (
       <div>
-        404
         <br />
-        {`${time}s later will go back`}
+        <div className={styles.container}>
+          <div className={styles['boo-wrapper']}>
+            <div className={styles.boo}>
+              <div className={styles.face} />
+            </div>
+            <div className={styles.shadow} />
+
+            <h1>
+              Whoops!
+            </h1>
+            <p>
+              We couldn&apos;t find the page you were looking for.
+              <br />
+
+              {`${time}s later will go back`}
+            </p>
+          </div>
+        </div>
       </div>
     );
   }
